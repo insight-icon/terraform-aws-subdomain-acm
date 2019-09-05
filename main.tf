809065594b9b6c90f5e49fa99d1c282de688d785
robc-io <robc.io.opensource@gmail.com>
2019-09-05 16:35:11 -0700

    adding output

--- main.tf
+++ main.tf
@@ -9,7 +9,8 @@ module "acm" {
   source  = "terraform-aws-modules/acm/aws"
   version = "~> v2.0"
 
-  domain_name  = var.root_domain_name
+//  domain_name  = var.root_domain_name
+  domain_name = "${var.subdomain}.${var.root_domain_name}"
   zone_id      = aws_route53_zone.subdomain.zone_id
 
   subject_alternative_names = [
---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "this_acm_certificate_arn" {
+  description = "The ARN of the certificate"
+  value       = module.acm.this_acm_certificate_arn
+}
