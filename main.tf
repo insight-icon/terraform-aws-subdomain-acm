14183ee19dad519e57c19cbf68f12130d77b785a
robc-io <robc.io.opensource@gmail.com>
2019-09-05 12:15:40 -0700

    init

---
+++ main.tf
@@ -0,0 +1,27 @@
+data "aws_caller_identity" "this" {}
+data "aws_region" "current" {}
+
+data "aws_route53_zone" "root" {
+  name = "${var.root_domain_name}."
+}
+
+resource "aws_route53_zone" "subdomain" {
+  name = "${var.subdomain}.${var.root_domain_name}."
+}
+
+
+module "acm" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> v2.0"
+
+  domain_name  = var.root_domain_name
+  zone_id      = aws_route53_zone.subdomain.zone_id
+
+  subject_alternative_names = [
+    "${var.subdomain}.${var.root_domain_name}"
+  ]
+
+  tags = {
+    Name = var.root_domain_name
+  }
+}
